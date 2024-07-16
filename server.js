7406cfa92a9e97b018ecac7856e816ac9625d6ed
eddie Hinojosa <eddie_hinojosa@live.com>
2024-07-16 11:30:46 -0400

    added database coneection and inital funciton for inquirer.prompt

--- server.js
+++ server.js
@@ -2,4 +2,39 @@ require("dotenv").config();
 const express = require("express");
 const inquirer = require("inquirer");
 const pg = require("pg");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const { Pool } = require("pg");
+
+//Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+//Connect to db with credentials from .env file
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+});
+
+pool.connect((err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("Connected to the database 🤘");
+        inputSelection();
+    }
+})
+
+function inputSelection() {
+    inquirer.prompt({
+
+
+
+        
+    })
+}
+
 
